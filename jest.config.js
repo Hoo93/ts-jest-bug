edc103cfddb891aad19ed211b25e20feac91697a
hoo93 <sksk8922@gmail.com>
2024-07-22 16:40:36 +0900

    feat: test, spec 모두 테스트하도록 수정

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/**/*.test.(ts|js)'],
+  testMatch: ['<rootDir>/test/**/*.(test|spec).(ts|js)'],
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
