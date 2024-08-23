4082f58fc5e9e3ab0cbde8c24858ad534980f769
Sang Hoo Park <sksk8922@gmail.com>
2024-08-23 10:46:29 +0900

    feat: minimal reproduction repository

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,12 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.(test|spec).(ts|js)'],
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': ['ts-jest',
+      {
+        // this option occurs an error
+        isolatedModules: true,
+     }
+    ]
   },
   coverageDirectory: 'coverage',
 };
--- test/init.test.ts
+++ test/init.test.ts
@@ -1,3 +1,10 @@
-describe('', () => {
-  
+describe('ts-jest error reporting', () => {
+  it('2=2', () => {
+    // Given
+    // When
+    const sut = 1+1
+
+    // Then
+    expect(sut).toBe(2)
+  });
 });
